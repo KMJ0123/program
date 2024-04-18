0f9a15b1266e0fdde115bad4026c327f1fcadae7
KMJ0123 <dhwow@naver.com>
2024-04-18 16:38:06 +0900

    g

--- 주식.py
+++ 주식.py
@@ -1,12 +1,20 @@
 #20244043 김민주
 print('20244043 김민주')
 
-import requests
+import requests, os
 from bs4 import BeautifulSoup
 from urllib import request, parse
 
 token = 'ZQAAAYEDIPngExdltTFMb-dmKO5rfejy7M3oHAvmKlpTY8pYinUnQ0C2cGVNjFG0k9yLSjHjC4uOony32LbEkWTGST9fPprrY3Sn6oT8beOaRaQf'
 band_key = 'AAAya6jGF4MUx5TXLm7eSun8'
+
+stock_check = int(input("""What stock would you like to check?
+                    1. Samsung 2. LG 3. SK
+                    Please enter the number: """))
+
+send = input("""How would you like to check stock information?
+             1. Band 2. file 3. Terminal
+             Please enter the number: """)
 
 def send_to_Band(bk, content, do_push=True):
     url = 'https://openapi.band.us/v2.2/band/post/create'
@@ -30,15 +38,11 @@ def get_stock_price(stock_code):
             return "주식 가격을 찾을 수 없습니다."
     else:
         return f"오류 발생: {response.status_code}"
-
-stock_check = int(input("""What stock would you like to check?
-                    1. Samsung 2. LG 3. SK
-                    Please enter the number: """))
-
+    
 stock_codes = ['005930', '003550', '017670']
 stock_names = ['삼성전자', 'LG', 'SK']
 
-if 1 <= stock_check <= len(stock_codes):
+if 1 <= stock_check <= len(stock_codes):    #원하는 주식 불러와주는 블럭
     stock_code = stock_codes[stock_check - 1]
     stock_name = stock_names[stock_check - 1]
     price = get_stock_price(stock_code)
@@ -46,15 +50,14 @@ if 1 <= stock_check <= len(stock_codes):
 else:
     print('Please choose a number from the options provided.')
 
-send = input("""How would you like to check stock information?
-             1. Band 2. file 3. Terminal
-             Please enter the number: """)
-
-if send == str(1):
+if send == str(1): #주식 자료를 어디서 확인할건지
     send_to_Band(band_key, f"{stock_name} 주식 가격: {price}")
 
 elif send == str(2):
-    with open("price.txt", "w") as f:
+    home_dir = os.path.expanduser("~")
+    desktop_path = os.path.join(home_dir, "Desktop")
+    stock = os.path.join(desktop_path, "price.txt")
+    with open(stock, "w") as f:
         f.write(f"{stock_name} 주식 가격: {price}")
 
 elif send == str(3):
@@ -62,7 +65,5 @@ elif send == str(3):
 
 else:
     print('Please choose a number from the options provided.')
-
-
 
     #주식 코드를 택스트 파일에 저장해서 실행할때 값을 가져와서 실행 여러개의 주식 값을 확인할 수 있도록 변경 그 주식을 내림차 순으로 정렬해서 출력
